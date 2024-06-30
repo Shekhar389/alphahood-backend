ed66684b0a3c571600292d0f58c73ca59ff03994
Rituraj Pandey <rp.weki.24@gmail.com>
2024-06-30 20:00:29 +0530

    PM2

---
+++ pm2.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+        apps: [
+                {
+                        name: "alphahood-backend",
+                        script: "npm",
+                        args: "start",
+                        env_file: ".env"
+                }
+        ]
+}
